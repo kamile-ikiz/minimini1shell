9a086af5ca36fecc38ace0b51c7bb40ad6a91a26
Kamile Ikiz <kikiz@k1m43s02.42istanbul.com.tr>
2025-07-15 14:28:36 +0300

    second

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kikiz <kikiz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:06:21 by kikiz             #+#    #+#             */
-/*   Updated: 2025/07/15 13:29:12 by kikiz            ###   ########.fr       */
+/*   Updated: 2025/07/15 14:28:19 by kikiz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,5 @@
 #include <unistd.h>
 
 int main() {
-    struct termios oldt, newt;
-    char ch;
 
-    // Mevcut terminal ayarlarını al
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-
-    // Yeni ayarları yap: canonical mode kapalı (satır satır değil, karakter karakter oku)
-    newt.c_lflag &= ~(ICANON | ECHO);  // ECHO kapalı → yazılan gösterilmez
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    printf("Bir tuşa bas (Enter gerekmiyor): ");
-    ch = getchar();
-
-    printf("\nBasılan karakter: %c\n", ch);
-
-    // Ayarları geri al
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    return 0;
 }
